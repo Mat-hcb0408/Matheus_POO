71aef3d79ec57e8420074c2b3b0145da71dc4ced
Matheus Henrique C Barbosa <matheus.henrique.campos0408@gmail.com>
2025-02-07 18:53:59 -0300

    Update PositivoNegativo.java

--- PositivoNegativo.java
+++ PositivoNegativo.java
@@ -7,16 +7,26 @@ public class PositivoNegativo {
         int x=sc.nextInt();
         sc.close();
         int resultado=PositivoNegativo(x);
-
+        ShowResult(resultado,x);
     }
     public static int PositivoNegativo(int x){
+        int aux;
         if (x>0){
-            System.out.printf(x+">0");
+            aux=0;
         }else if(x<0){
-            System.out.printf(x+"<0");
+            aux=1;
+        }else{
+            aux=2;
+        }
+        return aux;
+    }
+    public static void ShowResult(int value,int x){
+        if (value==0){
+            System.out.println(x+">0");
+        }else if(value==1){
+            System.out.println(x+"<0");
         }else{
             System.out.println(x+"=0");
         }
-        return x;
     }
 }
