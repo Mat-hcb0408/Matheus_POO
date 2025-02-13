6dd496183b84924f89bfb387d8cd6704a2e15c24
Matheus Henrique C Barbosa <matheus.henrique.campos0408@gmail.com>
2025-02-13 15:24:31 -0300

    Update Password.java

--- Password.java
+++ Password.java
@@ -1,7 +1,30 @@
-package App_password.Password;
+package App_password;
 
 public class Password {
-    private int CorrectPassword=1234;
-    public int password;
 
+    public String message;
+
+    public Password() {
+        this.message = "Insira sua senha:";
+    }
+
+    public String verifyPassword(int password) {
+        if (password == 1234) {
+            this.message = "Acesso liberado!";
+        } else {
+            this.message = "Acesso negado.";
+        }
+        return this.message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return "Password{" +
+                "message='" + message + '\'' +
+                '}';
+    }
 }
